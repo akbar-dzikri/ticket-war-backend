22a8563d0826c89ff4cb34f4c74dcce00564e1f5
akbar-dzikri <akbardzkr05@gmail.om>
2026-01-08 12:18:18 +0700

    Revert "chore: reorganize imports and register JWT plugin"
    This reverts commit e0c786cc5b78bbb41e9b6fa4f0375a739c774473.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,9 @@
-import "dotenv/config";
-
 import Fastify from "fastify";
 import { prisma } from "./lib/db.js";
-import fastifyJwt from "@fastify/jwt";
+
+
 const app = Fastify({ logger: true });
 
-app.register(fastifyJwt, {
-  secret: process.env["JWT_SECRET"] ?? "default_secret",
-});
 app.get("/", (request, reply) => {
   return { msg: "ok" };
 });
