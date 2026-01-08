5cc8228125f78743a761f254f335d4964ad99928
akbar-dzikri <akbardzkr05@gmail.om>
2026-01-09 05:04:55 +0700

    chore: setup pino-pretty for better log readability

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,25 @@
 import Fastify from "fastify";
 import jwt from "./plugins/jwt.js";
 
-const app = Fastify({ logger: true });
+const envToLogger = {
+  development: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+  production: true,
+  test: false,
+};
 
+const environment = (process.env.NODE_ENV || "production") as
+  | "development"
+  | "production"
+  | "test";
+const app = Fastify({ logger: envToLogger[environment] ?? true });
 app.register(jwt);
 
 app.get("/", (request, reply) => {
