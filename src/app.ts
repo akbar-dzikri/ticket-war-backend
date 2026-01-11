56c9cb86d45d11a5de2e129bee2ef5e91a418399
akbar-dzikri <akbardzkr05@gmail.com>
2026-01-11 22:27:40 +0700

    feat: add error handler to Fastify application

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import Fastify from "fastify";
 import jwt from "./plugins/jwt.js";
 import { authRoutes } from "./modules/auth/auth.route.js";
+import { errorHandler } from "./lib/error-handler.js";
 
 const envToLogger = {
   development: {
@@ -22,14 +23,15 @@ const environment = (process.env.NODE_ENV || "production") as
   | "test";
 const app = Fastify({ logger: envToLogger[environment] ?? true });
 
-await app.register(jwt)
+await app.register(jwt);
+
+app.setErrorHandler(errorHandler);
+
 await app.register(authRoutes, { prefix: "/api/auth" });
 
 app.get("/", (request, reply) => {
   return { msg: "ok" };
 });
-
-app.log.info(app.authenticate)
 
 app.get("/protected", { onRequest: [app.authenticate] }, (request, reply) => {
   return { msg: "you are auth'd", user: request.user };
