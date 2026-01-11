4670b6325107097bc80a28379d1272fb43cda0db
akbar-dzikri <akbardzkr05@gmail.com>
2026-01-11 22:28:34 +0700

    refactor: simplify handleLogin function by removing error handling

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -19,15 +19,10 @@ export async function handleLogin(
 ) {
   const body = request.body as OmitOptional<UserCreateInput>;
 
-  try {
-    const user = await authLoginService(body);
-    const token = await reply.jwtSign(
-      { id: user.id, email: user.email, role: user.role },
-      { expiresIn: "7d" }
-    );
-    reply.code(200).send({ token, user });
-  } catch (error) {
-    const statusCode = (error as any).statusCode || 500;
-    reply.code(statusCode).send({ error: (error as Error).message });
-  }
+  const user = await authLoginService(body);
+  const token = await reply.jwtSign(
+    { id: user.id, email: user.email, role: user.role },
+    { expiresIn: "7d" }
+  );
+  reply.code(200).send({ token, user });
 }
