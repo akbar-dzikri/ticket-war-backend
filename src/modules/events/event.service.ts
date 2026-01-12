e2b8dc5461158d077bb0e935301e95a64ff2dda8
akbar-dzikri <akbardzkr05@gmail.om>
2026-01-13 06:33:25 +0700

    feat: add a function to simulate payment gateway processing

--- src/modules/events/event.service.ts
+++ src/modules/events/event.service.ts
@@ -2,6 +2,14 @@ import { prisma } from "#/lib/db.js";
 import { EventCreateInput } from "#/lib/prisma/generated/models.js";
 import createHttpError from "http-errors";
 
+// Simulate payment gateway taking 1 - 3s for processing
+function simulatePaymentGateway() {
+  return new Promise((resolve) => {
+    const timeout = Math.floor(Math.random() * 1000) + 1000;
+    setTimeout(resolve, timeout);
+  });
+}
+
 export async function createEvent(payload: EventCreateInput, userId: number) {
   return await prisma.event.create({
     data: {
