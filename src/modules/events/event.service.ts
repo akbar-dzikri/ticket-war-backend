18777c81173ff009ffdb628f157f3847c28577ec
akbar-dzikri <akbardzkr05@gmail.om>
2026-01-13 13:09:49 +0700

    docs: booking logic comments and fix typo on payment gateway comment

--- src/modules/events/event.service.ts
+++ src/modules/events/event.service.ts
@@ -2,7 +2,7 @@ import { prisma } from "#/lib/db.js";
 import { EventCreateInput } from "#/lib/prisma/generated/models.js";
 import createHttpError from "http-errors";
 
-// Simulate payment gateway taking 1 - 3s for processing
+// Simulate payment gateway taking 1 - 2s for processing
 function simulatePaymentGateway() {
   return new Promise((resolve) => {
     const timeout = Math.floor(Math.random() * 1000) + 1000;
@@ -56,6 +56,8 @@ export async function getEventById(id: number) {
 }
 
 export async function bookEvent(eventId: number, userId: number) {
+
+  // Stock checking
   const event = await prisma.event.findUnique({
     where: {
       id: eventId,
@@ -66,6 +68,10 @@ export async function bookEvent(eventId: number, userId: number) {
     throw createHttpError(404, "Event not found");
   }
 
+
+  // the "bad" logic
+  // check if stock exist, then wait for 2s
+  // while the request is idle, other request might have already been completed
   if (event.availableTickets <= 0) {
     throw createHttpError(400, "No available seat");
   }
@@ -73,6 +79,8 @@ export async function bookEvent(eventId: number, userId: number) {
   console.log(`[userId: ${userId}] Processing payment...`);
   await simulatePaymentGateway();
 
+  // update stock
+  // this will (hopefully) still decrement even if the available ticket has already been 0
   const bookedEvent = await prisma.event.update({
     where: {
       id: eventId,
